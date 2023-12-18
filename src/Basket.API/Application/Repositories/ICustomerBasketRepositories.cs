f4817d36cdc4b9bb2863f05d3b9a678aff459880
hungnd2000 <fitthuctap42@fpts.com.vn>
2023-12-18 11:14:18 +0700

    new code

---
+++ src/Basket.API/Application/Repositories/ICustomerBasketRepositories.cs
@@ -0,0 +1,13 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Application.Repositories
+{
+    public interface ICustomerBasketRepositories
+    {
+        Task<List<CustomerBasket>> GetAllCustomerBasketAsync();
+        Task<CustomerBasket> GetCustomerBasketByIdAsync(int id);
+        Task<CustomerBasket> AddCustomerBasketAsync(CustomerBasket basket);
+        Task<CustomerBasket> UpdateCustomerBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteCustomerBasketAsync(int id);
+    }
+}
